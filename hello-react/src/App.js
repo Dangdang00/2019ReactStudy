ba927be2eb7e660a460e529fc2e973e167f346fe
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-12-28 20:31:22 +0900

    #5 2.4.2 자바스크립트 표현

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function App() {
+  const name = '다영';
   return (
     <>
-      <h1>리액트 안녕!</h1>
-      <h2>잘 작동하니?</h2>
+      <h1>{name} 안녕!</h1>
+      <h2>리액트는 잘 작동하니?</h2>
     </>
   );
 }
