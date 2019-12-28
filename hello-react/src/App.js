d2bb18eb2d62b438f0719f60f274ae3ea743e268
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-12-28 22:34:48 +0900

    #6 2.4.5 undefined를 렌더링하지 않기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  const name = '리액트';
-  return (
-    <div>
-      {/* {조건 ? (참일때) : (거짓일때)} */}
-      {/*{name === '리액트' ? <h1>리액트입니다.</h1> : null}*/}
-      {name === '리액트' && <h1>리액트입니다.</h1>}
-      {/* 0은 예외적으로 화면에 나타난다. */}
-    </div>
-  );
+  const name = undefined;
+  return <div>{name || '리액트'}</div>;
 }
 
 export default App;
