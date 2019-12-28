dddb699d45443cf0a245aa2e004598ab32e54ecd
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-12-28 22:06:51 +0900

    2.4.3 if문 대신 조건부 연산자

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,10 +3,14 @@ import React from 'react';
 function App() {
   const name = '다영';
   return (
-    <>
-      <h1>{name} 안녕!</h1>
-      <h2>리액트는 잘 작동하니?</h2>
-    </>
+    <div>
+      {/* {조건 ? (참일때) : (거짓일때)} */}
+      {name === '리액트' ? (
+        <h1>리액트입니다.</h1>
+      ) : (
+        <h2>리액트가 아닙니다.</h2>
+      )}
+    </div>
   );
 }
 
