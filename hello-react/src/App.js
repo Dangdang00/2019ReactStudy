a358f18a0b8d1c339c6775a00321b691d769c76f
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-12-31 20:13:07 +0900

    #9 3.1 클래스형 컴포넌트

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
 
-function App() {
-  const name = "리액트";
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-    </div>
-  )
+class App extends Component {
+  render() {
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
