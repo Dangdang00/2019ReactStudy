02de9dfe79d749f8d730677f99d5c39e7f93a8a0
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-01-04 21:20:47 +0900

    #11 3.3.4 태그 사이의 내용을 보여주는 children

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
