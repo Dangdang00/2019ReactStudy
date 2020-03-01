fe9a963b04b00eeb83072a8e208c9dc5561e4a5d
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-03-01 21:40:26 +0900

    #11 3.3.7 클래스형 컴포넌트에서 props 사용하기(1)

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children, favoriteNumber }) => {
-    return (
-        <div>
-            안녕하세요, 제 이름은 {name}입니다.<br/>
-            children값은 {children}입니다.<br/>
-            제가 좋아하는 숫자는 {favoriteNumber}입니다.
-        </div>
-    );
+class MyComponent extends Component {
+    render() {
+        const { name, favoriteNumber, children } = this.props; // 비구조화 할당
+    
+        return (
+            <div>
+                안녕하세요, 제 이름은 {name}입니다.<br/>
+                children값은 {children}입니다.<br/>
+                제가 좋아하는 숫자는 {favoriteNumber}입니다.
+            </div>
+        );
+    }
 };
 
 MyComponent.defaultProps = {
@@ -18,7 +22,6 @@ MyComponent.defaultProps = {
 MyComponent.propTypes = {
     name: PropTypes.string,
     favoriteNumber: PropTypes.number.isRequired
-    {/* 뒤에 isRequired를 붙여준다 */}
 };
 
 export default MyComponent;
