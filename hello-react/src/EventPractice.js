4475a4b67c68806909b22ea0810c306a28a2fb71
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-03-19 22:01:38 +0900

    4.2.2.1 onChange 이벤트 설정

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -5,6 +5,16 @@ class EventPractice extends Component {
         return (
             <div>
                 <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해보세요"
+                    onChange={
+                        (e) => {
+                            console.log(e.target.value);
+                        }
+                    }
+                />
             </div>
         );
     }
