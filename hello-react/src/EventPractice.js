400a84e03b1d81ddd79f2b1cc2430371c494dedd
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-03-31 22:41:48 +0900

    #14 4.3 함수형 컴포넌트로 구현해 보기(2)

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,14 +1,24 @@
 import  React, { useState } from 'react';
 
 const EventPractice = () => {
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const { username, message } = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form, // ...은 전개연산자, 기존의 form 내용을 이 자리에 복사한 뒤
+            [e.target.name]: e.target.value // 원하는 값을 덮어씌우기
+        };
+        setForm(nextForm);
+    };
     const onClick = () => {
         alert(username + ': ' + message);
-        setUsername('');
-        setMessage('');
+        setForm({
+            username: '',
+            message: ''
+        });
     };
     const onKeyPress = e => {
         if(e.key === 'Enter') {
@@ -23,14 +33,14 @@ const EventPractice = () => {
                 name="username"
                 placeholder="사용자명"
                 value={username}
-                onChange={onChangeUsername}
+                onChange={onChange}
             />
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력해 보세요"
                 value={message}
-                onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyPress={onKeyPress}
             />
             <button onClick={onClick}>확인</button>
