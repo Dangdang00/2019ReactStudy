dd8c5d4ae450d78554322d6e9adae9c4e08769ff
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-04-05 23:42:22 +0900

    5.3.3 컴포넌트에 메서드 생성 - 수정

--- hello-react/src/ScrollBox.js
+++ hello-react/src/ScrollBox.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class ScrollBox extends Component {
+
+    scrollToBottom = () => {
+        const { scrollHeight, clientHeight } = this.box; // 비구조화 할당 문법 사용
+        /*  위 코드와 같은 의미
+            const scrollHeight = this.box.scrollHeight;
+            const clientHeight = this.box.clientHeight;
+        */
+        this.box.scrollTop = scrollHeight - clientHeight;
+    }
+
     render() {
         const style = {
             border: '1px solid black',
@@ -14,15 +24,6 @@ class ScrollBox extends Component {
             width: '100%',
             height: '650px',
             background: 'linear-gradient(white, black)'
-        }
-
-        scrollToBottom = () => {
-            const{ scrollHeight, clientHeight } = this.Box; // 비구조화 할당 문법 사용
-            /*  위의 코드와 같은 의미
-                const scrollHeight = this.box.scrollHeight;
-                const clientHeight = this.box.clientHeight;
-            */
-           this.box.scrollTop = scrollHeight - clientHeight;
         }
 
         return (
