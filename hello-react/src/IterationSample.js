e5c52df80e0440f7bc47b429c68f199d321de4c4
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-05-03 23:06:59 +0900

    #16 6.3 key

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IterationSample = () => {
     const names = ['눈사람', '얼음', '눈', '바람']; // 문자열로 구성된 배열 선언
-    const nameList = names.map(name => <li>{name}</li>); // 배열 값을 사용해 JSX코드로 된 배열을 새로 생성한 후 nameList에 담기
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>); // 고유한 값이 없을 때만 index 값을 key로 사용해야함
     return <ul>{nameList}</ul>;
 };
 
