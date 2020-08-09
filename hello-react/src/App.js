b12643a40630c72686fa04235e140c8ff888b053
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-08-09 22:19:50 +0900

    7.3 라이프사이클 메서드 사용하기(2) - 에러 잡아내기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import LifeCycleSample from "./LifeCycleSample";
+import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -8,19 +9,21 @@ function getRandomColor() {
 class App extends Component {
   state = {
     color: '#000000'
-  }
+  };
 
   handleClick = () => {
     this.setState({
       color: getRandomColor()
     });
-  }
+  };
 
   render() {
     return(
       <div>
         <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color}/>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
       </div>
     );
   }
