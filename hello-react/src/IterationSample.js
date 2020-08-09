8b42a92c6eeb994675e3eedc1bf94e0c2f373d65
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-08-09 20:42:13 +0900

    6.4.2 데이터 추가 기능 구현하기

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -9,9 +9,24 @@ const IterationSample = () => {
     ]);
     const [inputText, setInputText] = useState('');
     const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 떄 사용할 id
-
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1); // nextId값에 1을 더해준다.
+        setNames(nextNames); // names값을 업데이트한다.
+        setInputText(''); // inputText를 비운다.
+    }
     const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
-    return <ul>{nameList}</ul>;
+    return (
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </>
+    )
 };
 
 export default IterationSample;
