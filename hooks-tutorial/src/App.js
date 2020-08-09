be77db1ff54d798445ce7d9553cde54c2cb6f566
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-08-09 22:39:45 +0900

    8.1.1 useState를 여러 번 사용하기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Counter from './Counter.js';
+import Info from './Info.js';
 
 const App = () => {
-  return <Counter />;
+  return <Info />;
 }
 
 export default App;
