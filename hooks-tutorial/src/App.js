eaf7452346e5440730f66597b5567301de5a6387
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-08-14 23:44:30 +0900

    8.2 useEffect

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import Info from './Info.js';
+import React, { useState } from 'react';
+import Info from './Info';
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr/>
+      {visible && <Info />}
+    </div>
+  )
 }
 
 export default App;
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        console.log('effect');
+        console.log(name);
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        };
+    });
 
     const onChangeName = e => {
         setName(e.target.value);
