b75687ec6d9561aada381ba0f9f062b907607b17
hwangdayoung <s2018s39@e-mirim.hs.kr>
2021-01-05 20:32:13 +0900

    14.2 axios로 API 호출해서 데이터 받아오기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [data, setData] = useState(null)
+  const onClick = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+      setData(response.data)
+    })
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+    </div>
   )
 }
 
